47d9d4d261c1112e5d21d58856e7e7f19969ec6f
Stedra Kristóf <stedra99@gmail.com>
2021-12-05 23:24:41 +0100

    Add static ip to node backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
-const { NODE_BACKEND_HOSTNAME } = process.env;
-
 export const environment = {
   production: true,
-  apiUrl: `${NODE_BACKEND_HOSTNAME}:3000/api/`
+  apiUrl: "10.5.0.2:3000/api/"
 };
